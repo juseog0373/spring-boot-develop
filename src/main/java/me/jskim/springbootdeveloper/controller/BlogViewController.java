ab2eae79a134f45d3cce52e78f16d24d86397b5d
keem <juseog0373@naver.com>
2023-07-14 23:04:35 +0900

    230714 블로그 게시물 수정, 등록, 삭제 기능추가

--- src/main/java/me/jskim/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/jskim/springbootdeveloper/controller/BlogViewController.java
@@ -3,11 +3,13 @@ package me.jskim.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.jskim.springbootdeveloper.domain.Article;
 import me.jskim.springbootdeveloper.dto.ArticleListViewResponse;
+import me.jskim.springbootdeveloper.dto.ArticleViewResponse;
 import me.jskim.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -32,7 +34,21 @@ public class BlogViewController {
     public String getArticle(@PathVariable Long id, Model model){
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleListViewResponse(article));
+
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model){
+
+        if(id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
 
